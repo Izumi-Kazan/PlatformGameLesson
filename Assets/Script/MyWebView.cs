5f4c32197c422b62a9dc4a0cde2669fa23be309d
Izumi-Kazan <hoge_no1@hotmail.com>
2015-06-27 19:06:00 +0900

    Assetsの追加

---
+++ Assets/Script/MyWebView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyWebView : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
